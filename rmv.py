3fe5d0c85decd7a70ccfffb851892666e6649f8c
zhangyingwei <zhangyw_001@126.com>
2022-06-06 08:58:07 +0800

    cm by me

--- rmv.py
+++ rmv.py
@@ -4,9 +4,11 @@ path='/app/youbili-bot/video'
 
 for video in os.listdir(path):
     vpath = os.path.join(path,video,"v.mp4")
+    dpath = os.path.join(path,video,"done")
   #  print(vpath)
     try:
-        os.remove(vpath)
-        print(vpath)
+        if os.path.exists(dpath):
+            os.remove(vpath)
+            print(vpath)
     except:
         pass
