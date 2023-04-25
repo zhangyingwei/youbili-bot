9640c8206c9b2a9699eb1e316450e5e75f419a1d
JoinWe <zhangyw_001@126.com>
2023-04-25 11:32:01 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,8 @@ git reset --hard origin/main
 git pull
 
 sed 's/=dev/=prod/g' conf.ini > conf.ini.1
-mv conf.ini.1 conf.ini -y
+rm -f conf.ini
+mv conf.ini.1 conf.ini
 mv -f /home/d_state.txt.1 ./d_state.txt
 git add .
 git commit -m 'commit by update'
