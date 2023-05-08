18c13ce58f2fa37d283780115427d020594db3ef
zhangyingwei <zhangyw_001@126.com>
2023-05-08 11:03:11 +0800

    添加删除分割线

--- rmv.py
+++ rmv.py
@@ -8,8 +8,13 @@ for video in os.listdir(path):
         if os.path.exists(dpath):
             print("+++++++++++++++++")
             for infile in os.listdir(os.path.join(path,video)):
-                os.remove(os.path.join(path,video,infile))
-                print(f"remove: {os.path.join(path,video,infile)}")
+                infile_path = os.path.join(path,video,infile)
+                if os.path.isdir(infile_path):
+                    os.rmdir(infile_path)
+                    print(f"remove dir: {infile_path}")
+                else:
+                    os.remove(infile_path)
+                    print(f"remove file: {os.path.join(path,video,infile)}")
             os.remove(os.path.join(path,video))
             print(f"remove: {os.path.join(path, video, infile)}")
             print("-----------------")
