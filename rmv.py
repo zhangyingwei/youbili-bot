9426a9f2ea21ff68e016356a9d97d34a83f1f646
zhangyingwei <zhangyw_001@126.com>
2023-05-08 11:20:40 +0800

    添加删除分割线

--- rmv.py
+++ rmv.py
@@ -2,6 +2,14 @@ import os
 
 path='/data/video'
 
+def try_delete_file(file_path):
+    if os.path.isdir(file_path):
+        os.rmdir(file_path)
+        print(f"remove dir: {file_path}")
+    else:
+        os.remove(file_path)
+        print(f"remove file: {file_path}")
+
 for video in os.listdir(path):
     dpath = os.path.join(path,video,"done")
     try:
@@ -9,19 +17,9 @@ for video in os.listdir(path):
             print("+++++++++++++++++")
             for infile in os.listdir(os.path.join(path,video)):
                 infile_path = os.path.join(path,video,infile)
-                if os.path.isdir(infile_path):
-                    os.rmdir(infile_path)
-                    print(f"remove dir: {infile_path}")
-                else:
-                    os.remove(infile_path)
-                    print(f"remove file: {os.path.join(path,video,infile)}")
-            os.remove(os.path.join(path,video))
-            print(f"remove: {os.path.join(path, video, infile)}")
+                try_delete_file(infile_path)
+            try_delete_file(os.path.join(path,video))
             print("-----------------")
     except Exception as e:
-        print(f"报错了: {e}. 尝试删除dir")
-        try:
-            os.rmdir(infile_path)
-            print(f"remove dir: {infile_path}")
-        except Exception as e2:
-            print(f"remove dir failed.{e2}")
+        print(f"报错了: {e}")
+        pass
